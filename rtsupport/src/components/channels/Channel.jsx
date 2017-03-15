352af65cb501dc4b87afd7480db8f74df94c1bbc
Christoph Jasinski <christoph.jasinski@googlemail.com>
2017-03-15 08:01:56 +0100

    Add Channel component

---
+++ rtsupport/src/components/channels/Channel.jsx
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+
+
+class Channel extends Component {
+    onClick(event) {
+        event.preventDefault();
+        const {setChannel, channel} = this.props;
+        setChannel(channel);
+    }
+
+    render() {
+        const channel = this.props.channel;
+
+        return (
+            <li>
+                <a onClick={this.onClick.bind(this)}>
+                    { channel.name }
+                </a>
+            </li>
+        );
+
+    }
+}
+
+Channel.propTypes = {
+    channel: React.PropTypes.object.isRequired,
+    setChannel: React.PropTypes.func.isRequired
+};
+
+export default Channel;
