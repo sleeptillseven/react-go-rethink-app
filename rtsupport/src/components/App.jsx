17ab583405f7d6a272a92195083ddd9c1c7ba749
Christoph Jasinski <christoph.jasinski@googlemail.com>
2017-03-18 16:32:10 +0100

    Include user and message components in app

--- rtsupport/src/components/App.jsx
+++ rtsupport/src/components/App.jsx
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
 import ChannelSection from './channels/ChannelSection';
+import UserSection from './users/UserSection';
+import MessageSection from './messages/MessageSection';
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
             channels: [],
+            users: [],
+            messages: [],
             activeChannel: {}
         };
     }
@@ -23,16 +27,40 @@ class App extends Component {
 
     }
 
+    setUserName(name) {
+        let {users} = this.state;
+        users.push({id: users.length, name});
+        this.setState({users});
+        // ToDo: Send to server
+    }
+
+    addMessage(body) {
+        let {messages, users} = this.state;
+        let createdAt = new Date();
+        let author = users.length > 0 ? users[0].name : 'anonymous';
+        messages.push({id: messages.length, body, createdAt, author});
+        this.setState({messages});
+        // ToDo: Send to Server
+    }
+
     render() {
         return (
-            <div className="app">
-                <div className="Nav">
+            <div className='app'>
+                <div className='nav'>
                     <ChannelSection
                         {...this.state}
                         setChannel={this.setChannel.bind(this)}
                         addChannel={this.addChannel.bind(this)}
                     />
+                    <UserSection
+                        {...this.state}
+                        setUserName={this.setUserName.bind(this)}
+                    />
                 </div>
+                <MessageSection
+                    {...this.state}
+                    addMessage={this.addMessage.bind(this)}
+                />
             </div>
         );
     }
